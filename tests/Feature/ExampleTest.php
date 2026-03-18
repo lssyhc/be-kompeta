6382509bd2d4908bbc09682835aeacdebc022ad9
Cahyo Susilo <lssyhc.study@gmail.com>
2026-03-18 12:12:50 +0700

    build(deps): upgrade laravel framework to v13 and fix api route test assertions

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,10 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+        ])->get('/api/user');
 
-        $response->assertStatus(200);
+        $response->assertStatus(401);
     }
 }
